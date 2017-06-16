653a105054933ea2ada6ef14de23c2c50eb9c9b0
Danielle <dnorris@sovenco.com>
2017-06-16 13:44:17 -0600

    Update section-hero.php

--- section-hero.php
+++ section-hero.php
@@ -1,20 +1,20 @@
 <?php
-$onepress_hero_id         = get_theme_mod( 'onepress_hero_id', esc_html__('hero', 'onepress') );
-$onepress_hero_disable    = get_theme_mod( 'onepress_hero_disable' ) == 1 ? true : false ;
-$onepress_hero_fullscreen = get_theme_mod( 'onepress_hero_fullscreen' );
-$onepress_hero_pdtop      = get_theme_mod( 'onepress_hero_pdtop', '10' );
-$onepress_hero_pdbotom    = get_theme_mod( 'onepress_hero_pdbotom', '10' );
+$sovenco_hero_id         = get_theme_mod( 'sovenco_hero_id', esc_html__('hero', 'sovenco') );
+$sovenco_hero_disable    = get_theme_mod( 'sovenco_hero_disable' ) == 1 ? true : false ;
+$sovenco_hero_fullscreen = get_theme_mod( 'sovenco_hero_fullscreen' );
+$sovenco_hero_pdtop      = get_theme_mod( 'sovenco_hero_pdtop', '10' );
+$sovenco_hero_pdbotom    = get_theme_mod( 'sovenco_hero_pdbotom', '10' );
 
-if ( onepress_is_selective_refresh() ) {
-    $onepress_hero_disable = false;
+if ( sovenco_is_selective_refresh() ) {
+    $sovenco_hero_disable = false;
 }
 
 $hero_content_style = '';
-if ( $onepress_hero_fullscreen != '1' ) {
-	$hero_content_style = ' style="padding-top: '. $onepress_hero_pdtop .'%; padding-bottom: '. $onepress_hero_pdbotom .'%;"';
+if ( $sovenco_hero_fullscreen != '1' ) {
+	$hero_content_style = ' style="padding-top: '. $sovenco_hero_pdtop .'%; padding-bottom: '. $sovenco_hero_pdbotom .'%;"';
 }
 
-$_images = get_theme_mod('onepress_hero_images');
+$_images = get_theme_mod('sovenco_hero_images');
 if (is_string($_images)) {
 	$_images = json_decode($_images, true);
 }
@@ -27,7 +27,7 @@ $images = array();
 
 foreach ( $_images as $m ) {
 	$m  = wp_parse_args( $m, array('image' => '' ) );
-	$_u = onepress_get_media_url( $m['image'] );
+	$_u = sovenco_get_media_url( $m['image'] );
 	if ( $_u ) {
 		$images[] = $_u;
 	}
@@ -37,7 +37,7 @@ if ( empty( $images ) ){
 	$images = array( get_template_directory_uri().'/assets/images/hero5.jpg' );
 }
 
-$is_parallax =  get_theme_mod( 'onepress_hero_parallax' ) == 1 && ! empty( $images ) ;
+$is_parallax =  get_theme_mod( 'sovenco_hero_parallax' ) == 1 && ! empty( $images ) ;
 
 if ( $is_parallax ) {
     echo '<div id="parallax-hero" class="parallax-hero parallax-window" >';
@@ -45,9 +45,9 @@ if ( $is_parallax ) {
 }
 
 ?>
-<?php if ( ! $onepress_hero_disable && ! empty ( $images ) ) : ?>
-	<section  id="<?php if ( $onepress_hero_id != '' ){ echo esc_attr( $onepress_hero_id ); } ?>" <?php if ( ! empty ( $images) && ! $is_parallax ) { ?> data-images="<?php echo esc_attr( json_encode( $images ) ); ?>"<?php } ?>
-             class="hero-slideshow-wrapper <?php echo ( $onepress_hero_fullscreen == 1 ) ? 'hero-slideshow-fullscreen' : 'hero-slideshow-normal'; ?>">
+<?php if ( ! $sovenco_hero_disable && ! empty ( $images ) ) : ?>
+	<section  id="<?php if ( $sovenco_hero_id != '' ){ echo esc_attr( $sovenco_hero_id ); } ?>" <?php if ( ! empty ( $images) && ! $is_parallax ) { ?> data-images="<?php echo esc_attr( json_encode( $images ) ); ?>"<?php } ?>
+             class="hero-slideshow-wrapper <?php echo ( $sovenco_hero_fullscreen == 1 ) ? 'hero-slideshow-fullscreen' : 'hero-slideshow-normal'; ?>">
 
         <div class="slider-spinner">
             <div class="double-bounce1"></div>
@@ -55,11 +55,11 @@ if ( $is_parallax ) {
         </div>
 
         <?php
-		$layout = get_theme_mod( 'onepress_hero_layout', 1 );
+		$layout = get_theme_mod( 'sovenco_hero_layout', 1 );
 		switch( $layout ) {
 			case 2:
-				$hcl2_content =  get_theme_mod( 'onepress_hcl2_content', wp_kses_post( '<h1>Business Website'."\n".'Made Simple.</h1>'."\n".'We provide creative solutions to clients around the world,'."\n".'creating things that get attention and meaningful.'."\n\n".'<a class="btn btn-secondary-outline btn-lg" href="#">Get Started</a>' ) );
-				$hcl2_image   =  get_theme_mod( 'onepress_hcl2_image', get_template_directory_uri().'/assets/images/onepress_responsive.png' );
+				$hcl2_content =  get_theme_mod( 'sovenco_hcl2_content', wp_kses_post( '<h1>Business Website'."\n".'Made Simple.</h1>'."\n".'We provide creative solutions to clients around the world,'."\n".'creating things that get attention and meaningful.'."\n\n".'<a class="btn btn-secondary-outline btn-lg" href="#">Get Started</a>' ) );
+				$hcl2_image   =  get_theme_mod( 'sovenco_hcl2_image', get_template_directory_uri().'/assets/images/sovenco_responsive.png' );
 				?>
 				<div class="container"<?php echo $hero_content_style; ?>>
 					<div class="hero__content hero-content-style<?php echo esc_attr( $layout ); ?>">
@@ -74,15 +74,15 @@ if ( $is_parallax ) {
 				<?php
 			break;
 			default:
-				$hcl1_largetext  = get_theme_mod( 'onepress_hcl1_largetext', wp_kses_post('We are <span class="js-rotating">OnePress | One Page | Responsive | Perfection</span>', 'onepress' ));
-				$hcl1_smalltext  = get_theme_mod( 'onepress_hcl1_smalltext', wp_kses_post('Morbi tempus porta nunc <strong>pharetra quisque</strong> ligula imperdiet posuere<br> vitae felis proin sagittis leo ac tellus blandit sollicitudin quisque vitae placerat.', 'onepress') );
-				$hcl1_btn1_text  = get_theme_mod( 'onepress_hcl1_btn1_text', esc_html__('Our Services', 'onepress') );
-				$hcl1_btn1_link  = get_theme_mod( 'onepress_hcl1_btn1_link', esc_url( home_url( '/' )).esc_html__('#services', 'onepress') );
-				$hcl1_btn2_text  = get_theme_mod( 'onepress_hcl1_btn2_text', esc_html__('Get Started', 'onepress') );
-				$hcl1_btn2_link  = get_theme_mod( 'onepress_hcl1_btn2_link', esc_url( home_url( '/' )).esc_html__('#contact', 'onepress') );
+				$hcl1_largetext  = get_theme_mod( 'sovenco_hcl1_largetext', wp_kses_post('We are <span class="js-rotating">sovenco | One Page | Responsive | Perfection</span>', 'sovenco' ));
+				$hcl1_smalltext  = get_theme_mod( 'sovenco_hcl1_smalltext', wp_kses_post('Morbi tempus porta nunc <strong>pharetra quisque</strong> ligula imperdiet posuere<br> vitae felis proin sagittis leo ac tellus blandit sollicitudin quisque vitae placerat.', 'sovenco') );
+				$hcl1_btn1_text  = get_theme_mod( 'sovenco_hcl1_btn1_text', esc_html__('Our Services', 'sovenco') );
+				$hcl1_btn1_link  = get_theme_mod( 'sovenco_hcl1_btn1_link', esc_url( home_url( '/' )).esc_html__('#services', 'sovenco') );
+				$hcl1_btn2_text  = get_theme_mod( 'sovenco_hcl1_btn2_text', esc_html__('Get Started', 'sovenco') );
+				$hcl1_btn2_link  = get_theme_mod( 'sovenco_hcl1_btn2_link', esc_url( home_url( '/' )).esc_html__('#contact', 'sovenco') );
 
-                $btn_1_style = get_theme_mod( 'onepress_hcl1_btn1_style', 'btn-theme-primary' );
-                $btn_2_style = get_theme_mod( 'onepress_hcl1_btn2_style', 'btn-secondary-outline' );
+                $btn_1_style = get_theme_mod( 'sovenco_hcl1_btn1_style', 'btn-theme-primary' );
+                $btn_2_style = get_theme_mod( 'sovenco_hcl1_btn2_style', 'btn-secondary-outline' );
 				?>
 				<div class="container"<?php echo $hero_content_style; ?>>
 					<div class="hero__content hero-content-style<?php echo esc_attr( $layout ); ?>">
