e0126e19ec21704d33722e04577304eb06899ad0
Danielle <dnorris@sovenco.com>
2017-06-16 15:16:08 -0600

    Update content-none.php

--- content-none.php
+++ content-none.php
@@ -4,27 +4,27 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package OnePress
+ * @package sovenco
  */
 
 ?>
 
 <section class="no-results not-found">
 	
-	<h2><?php esc_html_e( 'Nothing Found', 'onepress' ); ?></h2>
+	<h2><?php esc_html_e( 'Nothing Found', 'sovenco' ); ?></h2>
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'onepress' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'sovenco' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 		<?php elseif ( is_search() ) : ?>
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'onepress' ); ?></p>
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'sovenco' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php else : ?>
 
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'onepress' ); ?></p>
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'sovenco' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php endif; ?>
