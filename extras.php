fa827949480764c8eedad95034cb2b8a511cc5d9
Danielle <dnorris@sovenco.com>
2017-06-16 14:25:48 -0600

    Update extras.php

--- extras.php
+++ extras.php
@@ -4,7 +4,7 @@
  *
  * Eventually, some of the functionality here could be replaced by core features.
  *
- * @package OnePress
+ * @package sovenco
  */
 
 /**
@@ -13,21 +13,21 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function onepress_body_classes( $classes ) {
+function sovenco_body_classes( $classes ) {
 	// Adds a class of group-blog to blogs with more than 1 published author.
 	if ( is_multi_author() ) {
 		$classes[] = 'group-blog';
 	}
 
-	$onepress_sticky_header = get_theme_mod( 'onepress_sticky_header_disable' );
-	if ( $onepress_sticky_header == '' ) {
+	$sovenco_sticky_header = get_theme_mod( 'sovenco_sticky_header_disable' );
+	if ( $sovenco_sticky_header == '' ) {
 		$classes[] = 'sticky-header';
 	} else {
         $classes[] = 'no-sticky-header';
     }
-    // onepress_header_transparent
+    // sovenco_header_transparent
     if ( is_front_page() && is_page_template( 'template-frontpage.php' ) ) {
-        if ( get_theme_mod( 'onepress_header_transparent' ) ) {
+        if ( get_theme_mod( 'sovenco_header_transparent' ) ) {
             $classes[] = 'header-transparent';
         }
     }
@@ -38,28 +38,28 @@ function onepress_body_classes( $classes ) {
 
 	return $classes;
 }
-add_filter( 'body_class', 'onepress_body_classes' );
+add_filter( 'body_class', 'sovenco_body_classes' );
 
 
-if ( ! function_exists( 'onepress_custom_excerpt_length' ) ) :
+if ( ! function_exists( 'sovenco_custom_excerpt_length' ) ) :
 /**
  * Custom excerpt length for the theme
  */
-function onepress_custom_excerpt_length( $length ) {
+function sovenco_custom_excerpt_length( $length ) {
 	return 30;
 }
-add_filter( 'excerpt_length', 'onepress_custom_excerpt_length', 999 );
+add_filter( 'excerpt_length', 'sovenco_custom_excerpt_length', 999 );
 endif;
 
 
-if ( ! function_exists( 'onepress_new_excerpt_more' ) ) :
+if ( ! function_exists( 'sovenco_new_excerpt_more' ) ) :
 /**
  * Remove […] string using Filters
  */
-function onepress_new_excerpt_more( $more ) {
+function sovenco_new_excerpt_more( $more ) {
 	return ' ...';
 }
-add_filter('excerpt_more', 'onepress_new_excerpt_more');
+add_filter('excerpt_more', 'sovenco_new_excerpt_more');
 endif;
 
 
@@ -69,8 +69,8 @@ endif;
  * @param array $media
  * @return false|string
  */
-if ( ! function_exists( 'onepress_get_media_url' ) ) {
-    function onepress_get_media_url($media = array())
+if ( ! function_exists( 'sovenco_get_media_url' ) ) {
+    function sovenco_get_media_url($media = array())
     {
         $media = wp_parse_args($media, array('url' => '', 'id' => ''));
         $url = '';
@@ -100,20 +100,20 @@ if ( ! function_exists( 'onepress_get_media_url' ) ) {
  * 20 convert_smilies
  */
 global $wp_embed;
-add_filter( 'onepress_the_content', array( $wp_embed, 'run_shortcode' ), 8 );
-add_filter( 'onepress_the_content', array( $wp_embed, 'autoembed' ), 8 );
-add_filter( 'onepress_the_content', 'wptexturize' );
-add_filter( 'onepress_the_content', 'wpautop' );
-add_filter( 'onepress_the_content', 'shortcode_unautop' );
-add_filter( 'onepress_the_content', 'prepend_attachment' );
-add_filter( 'onepress_the_content', 'wp_make_content_images_responsive' );
-add_filter( 'onepress_the_content', 'capital_P_dangit' );
-add_filter( 'onepress_the_content', 'do_shortcode' );
-add_filter( 'onepress_the_content', 'convert_smilies' );
+add_filter( 'sovenco_the_content', array( $wp_embed, 'run_shortcode' ), 8 );
+add_filter( 'sovenco_the_content', array( $wp_embed, 'autoembed' ), 8 );
+add_filter( 'sovenco_the_content', 'wptexturize' );
+add_filter( 'sovenco_the_content', 'wpautop' );
+add_filter( 'sovenco_the_content', 'shortcode_unautop' );
+add_filter( 'sovenco_the_content', 'prepend_attachment' );
+add_filter( 'sovenco_the_content', 'wp_make_content_images_responsive' );
+add_filter( 'sovenco_the_content', 'capital_P_dangit' );
+add_filter( 'sovenco_the_content', 'do_shortcode' );
+add_filter( 'sovenco_the_content', 'convert_smilies' );
 
-if ( ! function_exists( 'onepress_get_layout' ) ) {
-    function onepress_get_layout( $default = 'right-sidebar' ) {
-        $layout = get_theme_mod( 'onepress_layout', $default );
+if ( ! function_exists( 'sovenco_get_layout' ) ) {
+    function sovenco_get_layout( $default = 'right-sidebar' ) {
+        $layout = get_theme_mod( 'sovenco_layout', $default );
         if ( function_exists( 'is_woocommerce' ) ) {
             if ( is_woocommerce() || is_cart() || is_checkout() || is_account_page() || is_wc_endpoint_url() ) {
                 $is_active_sidebar = is_active_sidebar( 'sidebar-shop' );
@@ -122,7 +122,7 @@ if ( ! function_exists( 'onepress_get_layout' ) ) {
                 }
             }
         }
-        return apply_filters( 'onepress_get_layout', $layout, $default );
+        return apply_filters( 'sovenco_get_layout', $layout, $default );
     }
 }
 
@@ -135,9 +135,9 @@ if ( ! function_exists( 'onepress_get_layout' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
     remove_action( 'woocommerce_cart_collaterals', 'woocommerce_cross_sell_display' );
 
-    add_filter( 'loop_shop_per_page', 'onepress_number_products_per_page', 20 );
+    add_filter( 'loop_shop_per_page', 'sovenco_number_products_per_page', 20 );
 
-    function onepress_number_products_per_page( $number ) {
+    function sovenco_number_products_per_page( $number ) {
         // $cols contains the current number of products per page based on the value stored on Options -> Reading
         // Return the number of products you wanna show per page.
         $number = 20;
