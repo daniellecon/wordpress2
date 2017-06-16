2826b2207d162914bb6b30b1dea9023ef4cf79c2
Danielle <dnorris@sovenco.com>
2017-06-16 15:14:15 -0600

    Update content-single.php

--- content-single.php
+++ content-single.php
@@ -4,7 +4,7 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package OnePress
+ * @package sovenco
  */
 
 ?>
@@ -14,7 +14,7 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 		<div class="entry-meta">
-			<?php onepress_posted_on(); ?>
+			<?php sovenco_posted_on(); ?>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
@@ -22,14 +22,14 @@
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'onepress' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sovenco' ),
 				'after'  => '</div>',
 			) );
 		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php onepress_entry_footer(); ?>
+		<?php sovenco_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
 
