f314263a371f1f6477f9a319086ddda434052b7e
Danielle <dnorris@sovenco.com>
2017-06-16 18:38:16 -0600

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
  *
  * @link https://codex.wordpress.org/Creating_an_Error_404_Page
  *
- * @package OnePress
+ * @package sovenco
  */
 
 get_header(); ?>
@@ -23,15 +23,15 @@ get_header(); ?>
 					<section class="error-404 not-found">
 
 						<div class="page-content">
-							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'onepress' ); ?></p>
+							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'sovenco' ); ?></p>
 
 							<?php get_search_form(); ?>
 
 							<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-							<?php if ( onepress_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+							<?php if ( sovenco_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
 							<div class="widget widget_categories">
-								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'onepress' ); ?></h2>
+								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'sovenco' ); ?></h2>
 								<ul>
 								<?php
 									wp_list_categories( array(
@@ -48,7 +48,7 @@ get_header(); ?>
 
 							<?php
 								/* translators: %1$s: smiley */
-								$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'onepress' ), convert_smilies( ':)' ) ) . '</p>';
+								$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'sovenco' ), convert_smilies( ':)' ) ) . '</p>';
 								the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 							?>
 
